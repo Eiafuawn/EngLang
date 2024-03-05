7388cf3b24f6874ccee9463fc56da142b7e5381a
Kenan Henzelin <kenan.henzelin@proton.me>
2024-03-05 11:45:49 +0100

    cleanup

--- lexer.cpp
+++ lexer.cpp
@@ -3,90 +3,95 @@
 #include <string>
 
 enum Token {
-    tok_eof = -1,
-    tok_fn = -2,
-    tok_end = -3,
-    tok_identifier = -4,
-    tok_number = -5,
-    tok_error = -6
+  tok_eof = -1,
+
+  // commands
+  tok_fn = -2,
+  tok_end = -3,
+
+  // primary
+  tok_identifier = -4,
+  tok_number = -5,
+
+  // errors
+  tok_error = -6
 };
 
 class Lexer {
 public:
-    Lexer() : LastChar(' ') {}
+  Lexer() : LastChar(' ') {}
 
-    Token getNextToken() {
-        while (isspace(LastChar))
-            LastChar = getchar();
+  Token getNextToken() {
+    while (isspace(LastChar))
+      LastChar = getchar();
 
-        if (isalpha(LastChar))
-            return tokenizeIdentifier();
+    if (isalpha(LastChar))
+      return tokenizeIdentifier();
 
-        if (isdigit(LastChar) || LastChar == '.')
-            return tokenizeNumber();
+    if (isdigit(LastChar) || LastChar == '.')
+      return tokenizeNumber();
 
-        if (LastChar == '#') {
-            skipComment();
-            return getNextToken();
-        }
-
-        if (LastChar == EOF)
-            return tok_eof;
-
-        // Return single character token
-        int ThisChar = LastChar;
-        LastChar = getchar();
-        return static_cast<Token>(ThisChar);
+    if (LastChar == '#') {
+      skipComment();
+      return getNextToken();
     }
+
+    if (LastChar == EOF)
+      return tok_eof;
+
+    // Return single character token
+    int ThisChar = LastChar;
+    LastChar = getchar();
+    return static_cast<Token>(ThisChar);
+  }
 
 private:
-    char LastChar;
+  char LastChar;
 
-    Token tokenizeIdentifier() {
-        std::string IdentifierStr;
-        do {
-            IdentifierStr += LastChar;
-            LastChar = getchar();
-        } while (isalnum(LastChar));
+  Token tokenizeIdentifier() {
+    std::string IdentifierStr;
+    do {
+      IdentifierStr += LastChar;
+      LastChar = getchar();
+    } while (isalnum(LastChar));
 
-        if (IdentifierStr == "fn")
-            return tok_fn;
-        if (IdentifierStr == "end")
-            return tok_end;
+    if (IdentifierStr == "fn")
+      return tok_fn;
+    if (IdentifierStr == "end")
+      return tok_end;
 
-        return tok_identifier;
-    }
+    return tok_identifier;
+  }
 
-    Token tokenizeNumber() {
-        std::string NumStr;
-        do {
-            NumStr += LastChar;
-            LastChar = getchar();
-        } while (isdigit(LastChar) || LastChar == '.');
+  Token tokenizeNumber() {
+    std::string NumStr;
+    do {
+      NumStr += LastChar;
+      LastChar = getchar();
+    } while (isdigit(LastChar) || LastChar == '.');
 
-        return tok_number;
-    }
+    return tok_number;
+  }
 
-    void skipComment() {
-        while (LastChar != EOF && LastChar != '\n' && LastChar != '\r')
-            LastChar = getchar();
-    }
+  void skipComment() {
+    while (LastChar != EOF && LastChar != '\n' && LastChar != '\r')
+      LastChar = getchar();
+  }
 };
 
-static void reportError(const char* Str) {
-    fprintf(stderr, "Error: %s\n", Str);
+static void reportError(const char *Str) {
+  fprintf(stderr, "Error: %s\n", Str);
 }
 
 int main() {
-    Lexer lexer;
-    Token tok;
-    while ((tok = lexer.getNextToken()) != tok_eof) {
-        if (tok == tok_error) {
-            reportError("Invalid token");
-            return 1;
-        }
-        printf("Token: %d\n", tok);
+  Lexer lexer;
+  Token tok;
+  while ((tok = lexer.getNextToken()) != tok_eof) {
+    if (tok == tok_error) {
+      reportError("Invalid token");
+      return 1;
     }
-    return 0;
+    printf("Token: %d\n", tok);
+  }
+  return 0;
 }
-
